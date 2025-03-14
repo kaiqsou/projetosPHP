1a67f881469b30c86bd6b4bef19ab1955765b016
Kaique <kaiqsouza2004@hotmail.com>
2025-03-14 09:11:55 -0300

    att

---
+++ atividades/exercicio2/somar_vetor.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 2. Somar Elementos de um Vetor
+   Crie um vetor com 5 números inteiros e some manualmente os valores acessando cada índice diretamente. 
+*/
+
+	$numeros = array(1, 2, 3, 4, 5);
+	$soma = 0;
+	$enumerador = 0;
+	
+	for ($i = 0; $i < 5; $i++)
+	{
+		$soma += $numeros[$i];
+		$enumerador = $i + 1;
+		echo "$enumerador"."º número: $numeros[$i] <br>";
+	}
+	
+	echo "Somando os números $numeros[0], $numeros[1], $numeros[2], $numeros[3], $numeros[4] teremos: $soma"
+
+?>
---
+++ atividades/exercicio3/atividade.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 3. Ordenação Manual de um Vetor
+   Crie um vetor de 3 números e ordene-os manualmente (sem usar sort()), comparando e trocando os valores, se necessário.
+*/
+
+	$numeros = array(2, 3, 1);
+	$ordem = array("", "", "");
+	$numAnterior = 0;
+	
+	echo "Números: <br>";
+	for($i = 0; $i < 3; $i++)
+	{
+		echo "$numeros[$i] <br><br>";
+	}
+	
+	echo "Ordem crescente: <br>";
+	
+	for($i = 0; $i < 3; $i++)
+	{
+		for($j = 0; $j < 3; $j++)
+		{
+			if($numeros[]
+		}
+	}
+	
+
+?>
+
