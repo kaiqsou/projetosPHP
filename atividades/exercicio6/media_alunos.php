7e3a8af9a5d7d3cdd9256d56934dec7cf9b464f8
Kaique <kaiqsouza2004@hotmail.com>
2025-03-17 09:31:06 -0300

    att

---
+++ atividades/exercicio6/media_alunos.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+6. Média de Notas dos Alunos
+Armazene as notas de 2 alunos (cada um com 3 notas) e exiba a média de cada um.
+
+*/
+
+$alunos = array
+		(
+		array("kaique", 10, 10, 10),
+		array("xiao", 5, 5, 5)
+		);
+		
+	echo "Aluno 1: $alunos[0][0], notas: $alunos[0][1], $alunos[0][2] e $alunos[0][3]"
+
+
+
+?>
