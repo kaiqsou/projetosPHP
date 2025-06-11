cc0f9dbdf1e4081834e9669982c67cd308c77bc2
Kaique <kaiqsouza2004@hotmail.com>
2025-06-10 23:27:26 -0300

    Arrumando a header

--- loja_mvc_kaique/views/header.php
+++ loja_mvc_kaique/views/header.php
@@ -7,27 +7,28 @@
 
 <!DOCTYPE html>
 <html>
-
 <head> 
 	<title>Loja</title>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
-	<script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 	
 <body> 
 	
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
 	  <a class="navbar-brand" href="#">Navbar</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	  </button>
 
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
+		<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 		
-		  <li class="nav-item active">
-			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		  <li class="nav-item">
+			<a class="nav-link active" aria-current="page" href="#">Home</span></a>
 		  </li>
 		  
 		  <?php 
@@ -48,7 +49,7 @@
 		?>
 		
 		<?php 
-			if(isset($_SESSION["nome"]))
+			if(!isset($_SESSION["nome"])) // se não tiver, executa o IF
 			{
 				echo "<li class='nav-item'>
 				<a class='nav-link' href='index.php?controle=categoriaController&metodo=login'>Entrar</a>
@@ -57,15 +58,16 @@
 			else
 			{
 				echo "<li class='nav-item'>
-				<a class='nav-link' href='index.php?controle=produtoController&metodo=logout'>Sair</a>
+				<a class='nav-link' href='index.php?controle=usuarioController&metodo=logout'>Sair</a>
 				</li>";
 			}
 		?>
 		</ul>
 		
-		<form class="form-inline my-2 my-lg-0">
-		  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		<form class="d-flex" role="search">
+		  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+		  <button class="btn btn-outline-success" type="submit">Search</button>
 		</form>
+		</div>
 	  </div>
 	</nav>
